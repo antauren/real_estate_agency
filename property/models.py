80ec472b0bd97a6bed24f280579ba506eb964d9c
antauren <antauren@yandex.ru>
2020-01-19 07:51:06 +0300

    add db_index

--- property/models.py
+++ property/models.py
@@ -25,7 +25,7 @@ class Flat(models.Model):
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField('Год постройки здания', null=True, blank=True, db_index=True)
 
-    new_building = models.NullBooleanField('Новостройка')
+    new_building = models.NullBooleanField('Новостройка', db_index=True)
 
     likers = models.ManyToManyField(User,
                                     verbose_name='Кому понравилось',
